2ced18f0e01ecb58369092d7e33b6affc7a73e72
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-06-29 22:07:15 -0400

    added button animations

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const playAudio = (url) => {
 
 function handleClick() {
     let buttonInnerHTML = this.innerHTML;
-    switch (buttonInnerHTML) {
+    makeSound(buttonInnerHTML)
+    buttonAnimation(buttonInnerHTML)
+}
+
+function handlePress(event) {
+    makeSound(event.key)
+    buttonAnimation(event.key)
+}
+
+function makeSound(key) {
+    switch (key) {
         case "w":
             playAudio("tom-1.mp3");
         break;
@@ -38,8 +48,16 @@ function handleClick() {
     }
 }
 
-for (let i = 0; i < 7; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+const buttonAnimation = (key) => {
+    let activeButton = document.querySelector('.' + key);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function() {activeButton.classList.remove('pressed')}, 100)
 }
 
+for (let i = 0; i < 7; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
+}
+
+document.addEventListener('keydown', handlePress)
